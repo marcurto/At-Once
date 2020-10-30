0045955e6aa1fb58ff68e665d3fa661c7a974c39
Cristina Castro <cristinacastro87@gmail.com>
2020-10-30 18:02:26 +0100

    cs

--- app.js
+++ app.js
@@ -11,26 +11,26 @@ const hbs = require('hbs');
 const mongoose = require('mongoose');
 
 mongoose
-  .connect('mongodb://localhost/proyecto-dos', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(x => {
-    console.log(
-      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+.connect( process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(x => {
+  console.log(
+    `Connected to Mongo! Database name: "${x.connections[0].name}"`
     );
   })
   .catch(err => {
     console.error('Error connecting to mongo', err);
   });
-
+  
+var app = express();
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var authRouter = require('./routes/auth');
 var restaurantRouter = require('./routes/restaurant');
 
 
-var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -43,9 +43,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
+app.use('/', authRouter)
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/', authRouter)
 app.use('/restaurant', restaurantRouter);
 
 // catch 404 and forward to error handler
