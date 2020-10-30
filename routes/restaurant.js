cc3f10627e5c51e5c5ecb03c4441903bc3b38c37
marcurto <mcurtogilabert@gmail.com>
2020-10-30 13:45:20 +0100

    edit and remove

--- routes/restaurant.js
+++ routes/restaurant.js
@@ -19,7 +19,7 @@ router.get("/dishes", withAuth, async (req, res, next) => {
     }
 });
 
-
+//Create dishes
 router.post("/dishes", withAuth, async (req, res, next) => {
     const dishInfo = {
       name: req.body.name,
@@ -41,5 +41,42 @@ router.post("/dishes", withAuth, async (req, res, next) => {
     });
   });
 
+//Edit dishes
+router.get('/dishes/edit/:id', withAuth, function (req, res, next) {
+  Dish.findOne({ _id: req.params.id }, (err, theDish) => {
+    if (err) { return next(err); }
+    res.render('restaurant/dishes', {
+      title: `Edit ${theDish.title}`,
+      dish: theDish
+    });
+  });
+});
+router.post('/dishes/edit/:id', function (req, res, next) {
+  const updatedDish= {
+    name: req.body.name,
+    description: req.body.description,
+    characteristics: req.body.characteristics,
+    allergies: req.body.allergies,
+    category: req.body.category
+  };
+  Dish.update({_id: req.params.id}, updatedDish, (err, theDish) => {
+    if (err) {return next(err); }
+    res.redirect('/restaurant/dishes');
+  });
+});
+
+
+
+
+  //Remove dishes
+  router.post('/dishes/remove/:id', withAuth, (req, res, next) => {
+    Dish.findByIdAndRemove(req.params.id)
+    .then((removedDish) => {
+      res.redirect('/restaurant/dishes');
+    })
+    .catch((error) => {
+        next();
+    })
+  });
 
 module.exports = router;
